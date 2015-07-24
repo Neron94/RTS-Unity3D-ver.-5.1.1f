66910a6e58beb9f66a6143ae95292e59dcfbc252
Neron94 <neron_venom@Mail.ru>
2015-07-24 22:55:51 +0500

    РТС контроллер готов!
    Импорт на камеру главное указать все ссылки.
    Указать Теги нужым обьектам

---
+++ Assets/Assets/Scripts/RTSController.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RTSController: MonoBehaviour {
+
+    
+    public GameObject Unit;
+    public Vector3 NachPoz;
+    public Vector3 TochNaz;
+    public float speed=10;
+	public Ray ray;
+	public RaycastHit hit;
+	public int range;
+	public Transform camera;
+	public bool asignUnit = false;
+    public bool move = false;
+
+
+	void Update()
+	{
+
+
+		if(Input.GetMouseButtonDown(0)){
+
+			ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			Physics.Raycast(ray, out hit, range);
+			Debug.DrawLine(camera.position, hit.point, Color.yellow);
+			    
+
+			     if(hit.collider.gameObject.tag==("Unit"))
+                     {
+				TochNaz = Unit.transform.position;
+				asignUnit = true;
+                NachPoz = Unit.transform.position;
+				Unit = hit.collider.gameObject;
+                     }
+                
+                if (hit.collider.gameObject.tag==("Terrain"))
+                         {
+				if(asignUnit== true)
+				{
+					TochNaz = hit.point;
+                    move = true;
+				}
+                         }
+
+		
+	                           		}
+		if (move == true){
+			Unit.transform.position= Vector3.MoveTowards(Unit.transform.position,  TochNaz, speed*Time.deltaTime);
+            if (Unit.transform.position == TochNaz)
+            {
+                NachPoz.Set(0, 0, 0);
+                TochNaz.Set(0, 0, 0);
+                move = false;
+            }
+                         }
+	
+		if(Input.GetMouseButtonDown(1)){
+		asignUnit = false;
+        NachPoz.Set(0,0,0);
+        TochNaz.Set(0, 0, 0);
+        move = false;
+		}
+	}
+}
+
+
+
+		
+
+
+       
